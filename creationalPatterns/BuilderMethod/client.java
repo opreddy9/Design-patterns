614f68aadf6c378f1fb02bf8ea88e7b348569f42
opreddy9 <omprakashreddy.j@gmail.com>
2025-04-09 18:09:10 +0530

    added some comments to make builder method immutable

--- creationalPatterns/BuilderMethod/client.java
+++ creationalPatterns/BuilderMethod/client.java
@@ -9,6 +9,8 @@ public class client {
         CarBuilder carBuilder=new CarBuilder();
         director.constructSedan(carBuilder);
         System.out.println("Sedan Car: "+carBuilder.getProduct().toString());
+        //here it is changing so it is not immutable if we want to make this immutable we have to make the variables final in the car
+        // and make constructor of car as private and make a builder class static inside the car class see an example of that
         carBuilder.setEngine("hehe");
         carBuilder.setGPS(false);
         System.out.println("Sedan Car: "+carBuilder.getProduct().toString());
